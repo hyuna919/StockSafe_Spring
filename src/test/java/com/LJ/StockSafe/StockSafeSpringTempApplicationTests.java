7386db873ec97a0d9426199e19ec5fab7384c5a5
simba155korea <simba155korea@gmail.com>
2021-12-12 16:26:10 +0900

    d

---
+++ src/test/java/com/LJ/StockSafe/StockSafeSpringTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LJ.StockSafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSafeSpringTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
