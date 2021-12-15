e78b2df442a762963d90c1dfa7e18232aa57044e
hyuna919 <hyuna919@naver.com>
2021-12-15 17:54:26 +0900

    UPDATE SotckMapper

--- src/main/java/com/LJ/StockSafe/model/mapper/StockMapper.java
+++ src/main/java/com/LJ/StockSafe/model/mapper/StockMapper.java
@@ -5,8 +5,9 @@ import java.util.List;
 import com.LJ.StockSafe.model.dto.StockDto;
 
 public interface StockMapper {
-	int createStock(String id, String stockName, String stockMarket, String stockSector, String stockWics);
-	List<StockDto> readStockAll();
+	int createStock(StockDto stock);
+	List<StockDto> readStockAll(String keyword);
 	StockDto readStockDetail(String id);
+	int updateStock(String id, StockDto stock);
 	int deleteStock(String id);
 }
