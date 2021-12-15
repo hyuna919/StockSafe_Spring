91b27061c024c487d7a09f629f0bd9c1eb535405
simba155korea <simba155korea@gmail.com>
2021-12-15 17:16:44 +0900

    REFACTOR : Change attributes to Camel case

--- src/main/java/com/LJ/StockSafe/model/dto/StockDetailDto.java
+++ src/main/java/com/LJ/StockSafe/model/dto/StockDetailDto.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class StockDetailDto {
-	private String stock_id;
-	private long stockdetail_marketcap;
-	private long stockdetail_count;
-	private float stockdetail_foreigner;
-	private float stockdetail_per;
-	private float stockdetail_pbr;
-	private float stockdetail_bps;
-	private float stockdetail_divyield;
+	private String stockId;
+	private long stockdetailMarketcap;
+	private long stockdetailCount;
+	private float stockdetailForeigner;
+	private float stockdetailPer;
+	private float stockdetailPbr;
+	private float stockdetailBps;
+	private float stockdetailDivyield;
 }
