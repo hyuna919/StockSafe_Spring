65c30ef5d4ca5ab13a55a8be441c7465903ffedd
hyuna919 <hyuna919@naver.com>
2021-12-16 20:37:25 +0900

    UPDATE swagger error

--- src/main/java/com/LJ/StockSafe/controller/UserinfoController.java
+++ src/main/java/com/LJ/StockSafe/controller/UserinfoController.java
@@ -3,6 +3,7 @@ package com.LJ.StockSafe.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,9 +28,8 @@ public class UserinfoController {
 
 //	List<UserinfoDto> readUserAll(String key, String word); 
 	@GetMapping
-	public ResponseEntity<List<UserinfoDto>> readUserAll(@ApiParam(value="유저 검색 기준") String key, @ApiParam(value="유저 검색 단어")String word){
-		userinfoService.readUserAll(key, word);
-		return null;
+	public ResponseEntity<List<UserinfoDto>> readUserAll(String key, String word){
+		return new ResponseEntity<List<UserinfoDto>>(userinfoService.readUserAll(key, word), HttpStatus.OK);
 	}
 
 }
