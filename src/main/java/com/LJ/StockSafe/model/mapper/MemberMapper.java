e5bdadaa046eb4f53f1cc7fdb4ef94c43d41472b
hyuna919 <hyuna919@naver.com>
2021-12-16 22:15:49 +0900

    UPDATE member mapper param

--- src/main/java/com/LJ/StockSafe/model/mapper/MemberMapper.java
+++ src/main/java/com/LJ/StockSafe/model/mapper/MemberMapper.java
@@ -3,15 +3,16 @@ package com.LJ.StockSafe.model.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.LJ.StockSafe.model.dto.MemberDto;
 
 @Mapper
 public interface MemberMapper {
 	int createUser(MemberDto userinfodto);
-	List<MemberDto> readUserAll(String key, String word); 
+	List<MemberDto> readUserAll(@Param("key") String key, @Param("word") String word); 
 	MemberDto readUser(String id);
-	String loginUser(String id, String userinfo_pw);
+	String loginUser(@Param("id") String id, @Param("userinfo_pw") String userinfo_pw);
 	int updateUser(String id);
 	int deleteUser(String id);
 }
