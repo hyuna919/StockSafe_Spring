8818910326fda2fb38a2cf1ee14e8c38efdeb4e9
이정현 <simba155korea@gmail.com>
2022-03-01 17:13:26 +0900

    UPDATE : priceDTO 구조 변경

--- src/main/java/com/LJ/StockSafe/model/dto/PriceDto.java
+++ src/main/java/com/LJ/StockSafe/model/dto/PriceDto.java
@@ -4,24 +4,39 @@ import java.math.BigDecimal;
 
 public class PriceDto {
 	BigDecimal now;
-	BigDecimal Low;
-	BigDecimal High;
+	BigDecimal low;
+	BigDecimal high;
 	BigDecimal prevClose;
 	BigDecimal open;
 	
-	public PriceDto(BigDecimal now, BigDecimal low, BigDecimal high, BigDecimal prevClose, BigDecimal open) {
+	BigDecimal marketCap; 
+	Long sharesOutstanding; //상장주식수
+	BigDecimal eps; //eps
+	BigDecimal pe; // PE
+	BigDecimal bps; //bps
+	BigDecimal annualYield;
+	
+	
+	public PriceDto() {}
+
+	public PriceDto(BigDecimal now, BigDecimal low, BigDecimal high, BigDecimal prevClose, BigDecimal open,
+			BigDecimal marketCap, Long sharesOutstanding, BigDecimal eps, BigDecimal pe, BigDecimal bps,
+			BigDecimal annualYield) {
 		super();
 		this.now = now;
-		Low = low;
-		High = high;
+		this.low = low;
+		this.high = high;
 		this.prevClose = prevClose;
 		this.open = open;
+		this.marketCap = marketCap;
+		this.sharesOutstanding = sharesOutstanding;
+		this.eps = eps;
+		this.pe = pe;
+		this.bps = bps;
+		this.annualYield = annualYield;
 	}
 	
-	public PriceDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	
 	public BigDecimal getNow() {
 		return now;
 	}
@@ -29,16 +44,16 @@ public class PriceDto {
 		this.now = now;
 	}
 	public BigDecimal getLow() {
-		return Low;
+		return low;
 	}
 	public void setLow(BigDecimal low) {
-		Low = low;
+		this.low = low;
 	}
 	public BigDecimal getHigh() {
-		return High;
+		return high;
 	}
 	public void setHigh(BigDecimal high) {
-		High = high;
+		this.high = high;
 	}
 	public BigDecimal getPrevClose() {
 		return prevClose;
@@ -52,5 +67,43 @@ public class PriceDto {
 	public void setOpen(BigDecimal open) {
 		this.open = open;
 	}
+	public BigDecimal getMarketCap() {
+		return marketCap;
+	}
+	public void setMarketCap(BigDecimal marketCap) {
+		this.marketCap = marketCap;
+	}
+	public Long getSharesOutstanding() {
+		return sharesOutstanding;
+	}
+	public void setSharesOutstanding(Long sharesOutstanding) {
+		this.sharesOutstanding = sharesOutstanding;
+	}
+	public BigDecimal getEps() {
+		return eps;
+	}
+	public void setEps(BigDecimal eps) {
+		this.eps = eps;
+	}
+	public BigDecimal getPe() {
+		return pe;
+	}
+	public void setPe(BigDecimal pe) {
+		this.pe = pe;
+	}
+	public BigDecimal getBps() {
+		return bps;
+	}
+	public void setBps(BigDecimal bps) {
+		this.bps = bps;
+	}
+	public BigDecimal getAnnualYield() {
+		return annualYield;
+	}
+	public void setAnnualYield(BigDecimal annualYield) {
+		this.annualYield = annualYield;
+	}
 	
+	
+
 }
