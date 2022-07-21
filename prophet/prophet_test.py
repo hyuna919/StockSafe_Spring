aed5fd20d3f2bd253aa69a3a8dbc24884fa107a0
simba155korea <simba155korea@gmail.com>
2022-07-21 16:56:48 +0900

    프로펫 스크립트(예측 후 이미지 생성 후 gcp 스토리지 업로드)

--- prophet/prophet_test.py
+++ prophet/prophet_test.py
@@ -4,25 +4,28 @@ from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
 from prophet.plot import add_changepoints_to_plot
 import pymysql
+import os
+from google.cloud import storage
 
 
 # DB에서 주식코드 받아오기
 ## DB연결
-connect = pymysql.connect(host='localhost', user='ssafy', password='ssafy', db='stocksafe_db', charset='utf8') # 한글처리 (charset = 'utf8')
+connect = pymysql.connect(host='localhost', user='root', password='ssafy', db='stocksafe_db', charset='utf8') # 한글처리 (charset = 'utf8')
 cur = connect.cursor()
 sql = "select id from stock"
 cur.execute(sql)
-list = cur.fetchall()
+list1 = cur.fetchall()
 
-print(list)
+print(list1)
 
 STOCK_CODE = "005930"
 
-for stock in list:
+for stock in list1:
     # 데이터 가져오기
     STOCK_CODE = stock[0]
     data = fdr.DataReader(STOCK_CODE)
 
+    print(2)
     # prophet사용 위한 전처리
     ## 반드시 y 컬럼과 ds 컬럼이 존재해야합니다.
     ## 예측 값은 y, 시계열 데이터는 ds에 지정합니다.
@@ -50,11 +53,36 @@ for stock in list:
     # https://zzsza.github.io/data/2019/02/06/prophet/
     fig = m.plot(forecast)
 
-    file_name = STOCK_CODE + ".png"
-    graph = m.plot(forecast).savefig(file_name)
+    file_name1 = "img/" + STOCK_CODE + ".png"
+    graph = m.plot(forecast).savefig(file_name1)
     # add_changepoints_to_plot(fig.gca(), m, forecast)
 
-    file_name = STOCK_CODE + "_seasonality.png"
-    graph_seasonality = m.plot_components(forecast).savefig(file_name)
+    file_name2 = "img/" + STOCK_CODE + "_seasonality.png"
+    graph_seasonality = m.plot_components(forecast).savefig(file_name2)
 
     # 이미지 저장 -> S3
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key/exalted-arcanum-356907-6273c406bf43.json"
+
+    print(1)
+
+    storage_client = storage.Client()
+    buckets = list(storage_client.list_buckets())
+
+    print(buckets)
+
+    # ----------------------------------------------------------
+    print(file_name1)
+
+    bucket_name = 'stocksafe_storage'    # 서비스 계정 생성한 bucket 이름 입력
+    source_file_name = file_name1    # GCP에 업로드할 파일 절대경로
+    destination_blob_name = file_name1    # 업로드할 파일을 GCP에 저장할 때의 이름
+
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('success')
